ab000d459db11c7e58ac6bf32427f99c846e0d32
Jiang Zhi <jiang.zhi@usp.br>
2018-04-07 19:22:01 -0300

    Commit

---
+++ src/data/trip.js
@@ -0,0 +1,151 @@
+export default {
+  "address_obj": {
+    "street1": "70 Rowes Wharf",
+    "street2": "",
+    "city": "Boston",
+    "state": "Massachusetts",
+    "country": "United States",
+    "postalcode": "02110-3354",
+    "address_string": "70 Rowes Wharf, Boston, MA 02110-3354"
+  },
+  "latitude": "42.356506",
+  "rating": "4.5",
+  "location_id": "89575",
+  "trip_types": [
+    {
+      "name": "business",
+      "value": "360",
+      "localized_name": "Business"
+    },
+    {
+      "name": "couples",
+      "value": "654",
+      "localized_name": "Couples"
+    },
+    {
+      "name": "solo",
+      "value": "75",
+      "localized_name": "Solo travel"
+    },
+    {
+      "name": "family",
+      "value": "441",
+      "localized_name": "Family"
+    },
+    {
+      "name": "friends",
+      "value": "102",
+      "localized_name": "Friends getaway"
+    }
+  ],
+  "write_review": "https://www.tripadvisor.com/UserReview-g60745-d89575-mMCID-Boston_Harbor_Hotel-Boston_Massachusetts.html",
+  "ancestors": [
+    {
+      "abbrv": null,
+      "level": "City",
+      "name": "Boston",
+      "location_id": "60745"
+    },
+    {
+      "abbrv": "MA",
+      "level": "State",
+      "name": "Massachusetts",
+      "location_id": "28942"
+    },
+    {
+      "abbrv": null,
+      "level": "Country",
+      "name": "United States",
+      "location_id": "191"
+    }
+  ],
+  "longitude": "-71.05025",
+  "hours": null,
+  "percent_recommended": null,
+  "hotel_booking": {
+    "bookable": true,
+    "booking_url": "https://www.tripadvisor.com/Hotel_Review-g60745-d89575-Reviews-mMCID-Boston_Harbor_Hotel-Boston_Massachusetts.html"
+  },
+  "review_rating_count": {
+    "1": "18",
+    "2": "22",
+    "3": "63",
+    "4": "269",
+    "5": "1408"
+  },
+  "subratings": [
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss5.0.gif",
+      "name": "rate_location",
+      "value": "5.0",
+      "localized_name": "Location"
+    },
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss4.5.gif",
+      "name": "rate_sleep",
+      "value": "4.5",
+      "localized_name": "Sleep Quality"
+    },
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss4.5.gif",
+      "name": "rate_room",
+      "value": "4.5",
+      "localized_name": "Rooms"
+    },
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss5.0.gif",
+      "name": "rate_service",
+      "value": "5.0",
+      "localized_name": "Service"
+    },
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss4.5.gif",
+      "name": "rate_value",
+      "value": "4.5",
+      "localized_name": "Value"
+    },
+    {
+      "rating_image_url": "https://static.tacdn.com/img2/ratings/traveler/ss5.0.gif",
+      "name": "rate_cleanliness",
+      "value": "5.0",
+      "localized_name": "Cleanliness"
+    }
+  ],
+  "ranking_data": {
+    "ranking_string": "#4 of 82 hotels in Boston",
+    "ranking_out_of": "82",
+    "geo_location_id": "60745",
+    "ranking": "4",
+    "geo_location_name": "Boston"
+  },
+  "photo_count": "477",
+  "location_string": "Boston, Massachusetts",
+  "web_url": "https://www.tripadvisor.com/Hotel_Review-g60745-d89575-Reviews-mMCID-Boston_Harbor_Hotel-Boston_Massachusetts.html",
+  "price_level": "$$$$",
+  "rating_image_url": "http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.5-MCID-5.svg",
+  "awards": [
+    {
+      "award_type": "Certificate of Excellence",
+      "year": "2016",
+      "images": {
+        "small": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_small-MCID-5.jpg",
+        "large": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_2016_en_US_large-MCID-5.jpg"
+      },
+      "categories": [],
+      "display_name": "Certificate of Excellence 2016"
+    }
+  ],
+  "name": "Boston Harbor Hotel",
+  "num_reviews": "1780",
+  "category": {
+    "name": "hotel",
+    "localized_name": "Hotel"
+  },
+  "subcategory": [
+    {
+      "name": "hotel",
+      "localized_name": "Hotel"
+    }
+  ],
+  "see_all_photos": "https://www.tripadvisor.com/Hotel_Review-g60745-d89575-mMCID-Reviews-Boston_Harbor_Hotel-Boston_Massachusetts.html#photos"
+}
