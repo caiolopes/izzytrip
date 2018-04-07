596f8974e0b723400329b8dd3a915db925d48a24
Caio Lopes <caiodesalopes@gmail.com>
2018-04-07 19:27:14 -0300

    Change searchbar

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import { Parallax } from 'react-parallax';
 import { withStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography';
-import AppSearch from '../components/AppSearch';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
 
 const styles = theme => ({
-  text: {
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
   },
   search: {
     height: 500,
@@ -13,6 +16,28 @@ const styles = theme => ({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textFieldRoot: {
+    padding: 0,
+    'label + &': {
+      marginTop: theme.spacing.unit * 3,
+    },
+  },
+  textFieldInput: {
+    borderRadius: 4,
+    backgroundColor: theme.palette.common.white,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 12px',
+    width: 'calc(100% - 24px)',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:focus': {
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+  textFieldFormLabel: {
+    fontSize: 18,
   },
 });
 
@@ -26,9 +51,27 @@ class SearchPage extends Component {
       <div>
         <Parallax bgImage={image} blur={{min: -1,max:3}}>
           <div className={classes.search}>
-            <Typography variant="display1" className={classes.text}>Aonde você quer ir?</Typography>
+            <Typography variant="display1">Aonde você quer ir?</Typography>
             <br />
-            <AppSearch />
+            <div className={classes.container}>
+              <TextField
+                placeholder="Digite uma cidade aqui"
+                id="bootstrap-input"
+                InputProps={{
+                  disableUnderline: true,
+                  classes: {
+                    root: classes.textFieldRoot,
+                    input: classes.textFieldInput,
+                  },
+                }}
+                InputLabelProps={{
+                  shrink: true,
+                  className: classes.textFieldFormLabel,
+                }}
+                style={{marginRight: 10, width: '60vw'}}
+              />
+              <Button variant="raised" color="primary">Busque intinerários</Button>
+            </div>
           </div>
         </Parallax>
       </div>
