9ee93c453d21d3b6519e2109c938b8286baa997c
Marcello <marcello_1000@yahoo.com.br>
2018-04-07 20:17:23 -0300

    ROuter pages

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
-import CssBaseline from 'material-ui/CssBaseline';
-
-import SearchPage from './pages/SearchPage';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import CssBaseline from "material-ui/CssBaseline";
+import SearchPage from "./pages/SearchPage";
+import Itinerary from "./pages/ItineraryPage";
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
         <CssBaseline />
-        <SearchPage />
+        <Router>
+          <div>
+            <Route exact path="/" component={SearchPage} />
+            <Route exact path="/it" component={Itinerary} />
+          </div>
+        </Router>
       </React.Fragment>
     );
   }
