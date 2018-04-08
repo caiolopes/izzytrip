7ce501a3dcfae08c052d894a799d6d960955f18e
Caio Lopes <caiodesalopes@gmail.com>
2018-04-08 03:42:31 -0300

    Fix swipe stack and add /it endpoint

--- react-ui/src/components/SwipeDeck.js
+++ react-ui/src/components/SwipeDeck.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Typography from 'material-ui/Typography';
 import Cards, { Card } from "react-swipe-card";
 import * as api from '../api';
 import "./styles.css";
@@ -10,16 +11,7 @@ class SwipeDeck extends Component {
 
   componentDidMount() {
     api.getPlaces('').then(res => {
-      const data = res.data.map((place, index) => {
-        console.log(place.image);
-        if (place.image) {
-          return {
-            ...place,
-            backgroundImage: `url(${place.image})`
-          };
-        }
-      });
-      this.setState({ data });
+      this.setState({ data: res.data });
     });
   }
 
@@ -36,11 +28,13 @@ class SwipeDeck extends Component {
               onSwipeLeft={() => console.log("left")}
               onSwipeRight={() => console.log("right")}
               key={key}
-              style={{backgroundImage: item.backgroundImage}}
             >
-              <div className="Title">
-                <h2>{item.name}</h2>
-                <p>{item.local}</p>
+              <div className="place-container">
+                <img src={item.image} />
+                <div className="overlay-title">
+                  <Typography variant="title" style={{color: 'white'}}>{item.name}</Typography>
+                  <Typography>{item.local}</Typography>
+                </div>
               </div>
             </Card>
           ))}
