3f53828f65693959e3d5ba83e507a701ea093a2d
Marcello <marcello_1000@yahoo.com.br>
2018-04-08 00:19:47 -0300

    Bug in create it

--- react-ui/src/pages/BuildItineraryCityPage.js
+++ react-ui/src/pages/BuildItineraryCityPage.js
@@ -1,18 +1,9 @@
 import React, { Component, Fragment } from "react";
-import SearchPage from './SearchPage';
-import image from './beach.jpg';
+import Tinder from "../components/Tinder.js";
 
 class BuildItineraryCityPage extends Component {
   render() {
-    return (
-      <Fragment>
-        <SearchPage
-          headline="Organize sua viagem"
-          buttonText="Criar"
-          pathname="build/it/new"
-          image={image} />
-      </Fragment>
-    );
+    return <div> oi </div>;
   }
 }
 
