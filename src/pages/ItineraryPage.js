04a38baeb6ce2c6abb672530b0afe52bbe2542bf
Marcello <marcello_1000@yahoo.com.br>
2018-04-07 21:45:31 -0300

    pagina do itinerario

--- src/pages/ItineraryPage.js
+++ src/pages/ItineraryPage.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
+import Maps from "../components/Maps.js";
+
+import "./ItineraryPage.css";
 
 class Itinerary extends Component {
   render() {
-    return <div> OI </div>;
+    return (
+      <div>
+        <div className="split left">
+          <Maps />
+        </div>
+        <div className="split right">
+          <div>
+            <img
+              className="image_preview"
+              src="http://alaya.com.br/wp-content/uploads/2014/11/nascente-areia-que-canta-brotas.jpg"
+              alt="sei la"
+            />
+          </div>
+          <div className="content">
+            <div>
+              <h2>Muchilão lou cost</h2>
+              <div className="line" />
+            </div>
+            <div>
+              <p>
+                Bacon ipsum dolor amet pancetta andouille sirloin, bacon
+                leberkas pork chop tri-tip prosciutto capicola cupim drumstick
+                fatback pastrami. Cupim sirloin beef ribs andouille. Shoulder
+                porchetta turkey tenderloin beef, boudin kielbasa prosciutto
+                chicken pancetta ribeye buffalo. Hamburger kevin sausage picanha
+                ball tip kielbasa.
+              </p>
+            </div>
+            <div className="line" />
+            <div className="info"> 200 reais, 4 horas</div>
+            <div className="line" />
+            <div className="user"> User </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
